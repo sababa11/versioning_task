8b4c241e87fc0352a81a521bc7919c79cea806d3
adamt <adamtarasov@gmail.com>
2020-08-30 11:38:05 +0300

    adding two setup.py for each service

---
+++ setup2.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='Versioning Task',
+    version='0.1',
+    description='Python Versioning Task',
+    author='Adam Ta',
+    author_email='notrelevant@mail.net',
+    url='https://github.com/sababa11/versioning_task.git',
+    install_requires=['boto3==1.14.51', 'requests==2.24.0'],
+    packages=['p1', 'p2', 'p3'],
+    package_dir={'p1': 'src1/p1', 'p2': 'src2/p2', 'p3': 'src3/p3'}
+    )
+
+print("OK")
